6edaa385cd903c8b278a051b7849c07be97eb5cb
yusme <yeimy.valencia1@outlook.de>
2018-02-19 22:51:52 +0000

    Update connectionMatlab.py

--- connectionMatlab.py
+++ connectionMatlab.py
@@ -9,6 +9,10 @@ Created on Mon Jan 29 12:10:12 2018
 
 import matlab.engine
 import matplotlib.pyplot as plt
+import connectionMatlab1 as CM
+
+
+
 
 
 class ConnectionMatlab:
@@ -22,27 +26,29 @@ class ConnectionMatlab:
     
     
     
-    def plotResults(self,x,y):     
-         plt.figure(1)
+    def plotResults(self,x,y):       
          plt.plot(x,y)
          plt.show();
        
+        
        
     
     
     def Openconnection(self):         
         eng = matlab.engine.start_matlab()
-        ret = eng.sqrt(4.0)        
+        ret = eng.sqrt(4.0)
+        
         if(ret==2.0):
             print('Run connection---Done!', ret)        
             self.eng = eng; 
-      
+            
+            
+            
         
-    def LoadInputData(self, inputData):
+        
+    def LoadInputData(self):
         eng = self.eng 
-        cad = "load('"+inputData+"')"
-        print (cad)
-        eng.eval(cad,nargout=0);
+        eng.eval("load('input.mat')",nargout=0);
         var =eng.workspace['var']
                
         print ("\n")  
@@ -51,134 +57,120 @@ class ConnectionMatlab:
         
         
         
+        
+        
+        
     # simulation 1
     def runSimulation(self,model, times):
     
         print("runSimulation" );    
        
-        eng = self.eng   
-        eng.eval("sim('"+model+"')");  
+        eng = self.eng            
         
-        StartTime = times["StartTime"];          
-        StopTime  = times["StopTime"];         
-        N =  len(times["StartTime"])
+         
+        eng.eval("sim('"+model+"')");
         
-        for n in range(0,N):    
+       
+         # set Parameter für simulation
+        eng.eval("set_param('"+model+"','StartTime','0','StopTime','80')",nargout=0);
+        
+           
+        StartTime = times["StartTime"]  ;          
+        StopTime  = times["StopTime"];          
+        
+        for n in range(0,2):    
             
             
-           cad =cad ="set_param('"+model+"','StartTime','"+ str(StartTime[n])+"', 'StopTime' ,'"+ str(StopTime[n])+"')";        
+           cad ="set_param('"+model+"','StartTime','"+ str(StartTime[n])+"', 'StopTime' ,'"+ str(StopTime[n])+"')";
           
            print ("--",  cad)
            
-           # set Parameter 
+           # set Parameter für simulation
            eng.eval(cad,nargout=0)
            
            print ("'StartTime:'",  eng.get_param(model,'StartTime') )
-           print ("'StopTime:'",  eng.get_param(model,'StopTime') )            
+           print ("'StopTime:'",  eng.get_param(model,'StopTime') )
+             
            
            
-           # run Simulation 
-           eng.eval("sim('"+model+"')");         
+           eng.eval("sim('"+model+"')");
+           var =eng.workspace['ScopeData']
+           #x = var['time']
+           #y = var['signals']['values']
+             
            
-           var={}
-           var=eng.workspace['ScopeData'] 
-           print ("ScopeData:", "\n\n:",var) 
-        
+           #print ('ScopeData: ', var)
+          
             
-           # run algorithmus
-           self.runAlgorithmus(var)
-            
+           #algorithmus
+           #self.plotResults(x,y) 
+           print ("\n ENDE")  
            
-           print ("\n")  
            
-        
-    def runAlgorithmus(self,var):
-        x = var['time']
-        y = var['signals']['values']
-        #print ('ScopeData: ', var,"\n\n")   
-        self.plotResults(x,y) 
-        
-        
+           
+           
+           
            
      # simulation 2  with input data     
     def runSimulationInput(self,model):           
         eng = self.eng  
         eng.eval("sim('"+model+"')");
-        
-        
         var =eng.workspace['ScopeData']
         x = var['time']
         y = var['signals']['values']
         self.plotResults(x,y) 
     
     
-   
     
-    def CloseConnection(self):         
-        self.eng.quit()
-        print('Close connection---Done!')        
-           
-            
     
  
+    
+    
+    
+def main():    
+   
+    model ='simu'
+    #result="ScopeData"     
    
     
+    #var={};
+    times={};
+    times["StartTime"] = [ 0.0,10.0,30.0,80.0,110.0];    
+    times["StopTime"] = [ 10.0,30.0,80.0,110.0,250.0] ;    
+   
+    print ("\n times ", times["StartTime"]," *** ",  times["StopTime"])
+    connection = CM.ConnectionMatlab()      
+    connection.Openconnection();
+    #connection.runSimulation(model,times);
 
 
 
+    #========
+    print ("\n first simulation Done!  \n\n")
+     
+    model ='data_import'
+    connection.LoadInputData();
+    connection.runSimulationInput(model);
+    
 
+    
+    
+    print ("\n second simulation  Done! \n\n")
+    
+    times["StartTime"] = [0.0,0.0];    
+    times["StopTime"] = [0.06,0.09]
+    
+    model ='sldemo_h'
+    connection.runSimulation(model,times);
+    
+    print ("\n End third simulation Done! \n\n")
+    #var={};
+    #signals={};
+    #signals["values"] = [0, 0, 2, 2, 2, 3, 3, 3];    
+    #signals["dimensions"] = 1;    
+    
+    
+       
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#if __name__ == '__main__':
+main() 
